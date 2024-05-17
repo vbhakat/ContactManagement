a4e76d7af824fba2b5f0b9630790fa2405612081
vbhakat <venkateshbhakat@outlookc.com>
2024-05-17 15:40:39 +0530

    fix: button click form not rendering

--- contacts-app/src/ContactForm/index.tsx
+++ contacts-app/src/ContactForm/index.tsx
@@ -1,8 +1,8 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-export interface ContactFormProps{
-    contactToUpdate:Contact|null;
-    onSubmit:(contact:Contact)=>void;
+export interface ContactFormProps {
+    contactToUpdate: Contact | null;
+    onSubmit: (contact: Contact) => void;
 }
 export interface Contact {
     id: number;
@@ -12,71 +12,67 @@ export interface Contact {
     address: string;
 }
 
-const ContactForm: React.FC<ContactFormProps>=({contactToUpdate,onSubmit}) =>{    
-    const [contact,setContact]=useState<Contact>(
+const ContactForm: React.FC<ContactFormProps> = ({ contactToUpdate, onSubmit }) => {
+    const [contact, setContact] = useState<Contact>(
         {
-            id:0,
-            name:'',
-            email:'',
-            phone:'',
-            address:'',
+            id: 0,
+            name: '',
+            email: '',
+            phone: '',
+            address: '',
         });
-        useEffect(()=>{
-            if(contactToUpdate)
-                {
-                   setContact(contactToUpdate); 
-                }
-        },[contactToUpdate]);
-        const handleInputChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
-            setContact({
-                ...contact,
-                [e.target.name]:e.target.value,
-            });
-        };
-        const handleSubmit=(e:React.FormEvent)=>{
-            e.preventDefault();
-            onSubmit(contact);
-        };
-        return (
-            <div className="modal">
-                <div className="modal-content">
-                    {/* <span className="close" onClick={onClose}>&times;</span> Close button */}
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <input
-                                type='text'
-                                name="name"
-                                value={contact.name}
-                                onChange={handleInputChange}
-                                placeholder="Name"
-                            />
-                            <input
-                                type='email'
-                                name="email"
-                                value={contact.email}
-                                onChange={handleInputChange}
-                                placeholder="Email"
-                            />
-                            <input
-                                type='text'
-                                name="phone"
-                                value={contact.phone}
-                                onChange={handleInputChange}
-                                placeholder="Phone"
-                            />
-                            <input
-                                type='text'
-                                name="address"
-                                value={contact.address}
-                                onChange={handleInputChange}
-                                placeholder="Address"
-                            />
-                            <button type='submit'>Add New Contact</button>
-                        </div>
-                    </form>
-                </div>
+    useEffect(() => {
+        if (contactToUpdate) {
+            setContact(contactToUpdate);
+        }
+    }, [contactToUpdate]);
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setContact({
+            ...contact,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        onSubmit(contact);
+    };
+    return (
+
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input
+                    type='text'
+                    name="name"
+                    value={contact.name}
+                    onChange={handleInputChange}
+                    placeholder="Name"
+                />
+                <input
+                    type='email'
+                    name="email"
+                    value={contact.email}
+                    onChange={handleInputChange}
+                    placeholder="Email"
+                />
+                <input
+                    type='text'
+                    name="phone"
+                    value={contact.phone}
+                    onChange={handleInputChange}
+                    placeholder="Phone"
+                />
+                <input
+                    type='text'
+                    name="address"
+                    value={contact.address}
+                    onChange={handleInputChange}
+                    placeholder="Address"
+                />
+                <button type='submit'>Add New Contact</button>
             </div>
-        )
+        </form>
+
+    )
 }
 
 export default ContactForm;
